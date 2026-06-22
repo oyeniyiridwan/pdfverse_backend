55a39b731dc1f946e17d28192499657a59c3b062
Your Name <oyeniyiridwan5@gmail.com>
2026-06-22 02:47:28 +0100

    refactor: update redis connection logic to use get_multiplexed_async_connection and clean up imports

--- src/infrastructure/redis.rs
+++ src/infrastructure/redis.rs
@@ -1,15 +1,10 @@
+use redis::{RedisError, aio::MultiplexedConnection};
 
-
-
-
-use redis::{Client, aio::MultiplexedConnection, RedisError};
-
-pub async fn redis_database_connection(
-    redis_url: &str
-) -> Result<MultiplexedConnection, RedisError> {
-    let client = Client::open(redis_url)?;
-
-    let conn = client.get_multiplexed_tokio_connection().await?;
-
-    Ok(conn)
+pub async fn redis_database_connection(redis_url: &str)->Result<MultiplexedConnection, RedisError>{
+ let redis_client = redis::Client::open(redis_url)?;
+redis_client
+        .get_multiplexed_async_connection()
+        .await
 }
+
+
